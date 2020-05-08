06fc048017dc4f6d009fcd1ddd5f3989c465214d
Natalia <n.b.urbanska@gmail.com>
2020-05-08 12:30:42 +0200

    post create/delete/update, pagination

--- web/urls.py
+++ web/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='web-home'),
-    path('user/<str:username>', UserPostListView.as_view(), name='user-post'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('about/', views.about, name='web-about'),
